9632c0900e8b78fc4b28d4ddaf6207fdb74426cf
Zeljko Maric <113950802+zeljkomaric95@users.noreply.github.com>
2023-03-20 19:21:13 -0600

    Update Account.jsx
    Signed-off-by: Zeljko Maric <113950802+zeljkomaric95@users.noreply.github.com>

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -9,7 +9,7 @@ const Account = ({ user }) => {
                 <div className="sm:flex sm:items-center sm:justify-between">
                 <div className="text-center sm:text-left">
                     <h1 className="font-serif text-2xl font-bold text-gray-900 sm:text-3xl">
-                            Welcome Back, {user?.username}!
+                            Welcome back, {user?.username}!
                     </h1>
 
                     <p className="mt-1.5 text-sm text-gray-500">
